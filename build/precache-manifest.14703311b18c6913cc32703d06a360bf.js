a51b5326889daa1766d6549f9d6ae8863e5a7251
asuradev128 <asuradev128@gmail.com>
2022-08-23 20:43:53 +0800

    first commit

---
+++ build/precache-manifest.14703311b18c6913cc32703d06a360bf.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e2a193d2598b98cfb821e9e7a7d62f82",
+    "url": "/index.html"
+  },
+  {
+    "revision": "740f25e12ee2134f6852",
+    "url": "/static/js/2.b34642aa.chunk.js"
+  },
+  {
+    "revision": "a27a10e52e3d9ffc0e4a7c94b342b1f1",
+    "url": "/static/js/2.b34642aa.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fd9c0385a052508869db",
+    "url": "/static/js/main.fcdc1a4b.chunk.js"
+  },
+  {
+    "revision": "4e0e34f265fae8f33b01b27ae29d9d6f",
+    "url": "/static/js/main.fcdc1a4b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b0ff50f5219b612b5fa3",
+    "url": "/static/js/runtime-main.3ab34bba.js"
+  }
+]);
